27d550b00192674705a66fd3983b918eed04ba5d
Nathan <nathan@users.noreply.github.com>
2017-07-17 09:34:28 -0400

    Add print_usage

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::env;
+
+fn print_usage() -> ! {
+    unimplemented!();
+}
+
 fn main() {
+    let file_name = match env::args().nth(1) {
+        Some(f) => f,
+        None => print_usage(),
+    };
     unimplemented!();
 }
