d47f8df566ee693b206c56d4a2e18280d85ffb42
Nathan <nathan@users.noreply.github.com>
2017-07-17 09:26:56 -0400

    Add some options

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,24 @@
-pub fn parse_script(input: &str) -> ! {
+pub struct ParseOptions<'a> {
+    pub first_line: usize,
+    pub file_name: &'a str,
+}
+
+impl<'a> Default for ParseOptions<'a> {
+    fn default() -> Self {
+        ParseOptions {
+            first_line: 0,
+            file_name: "<input>",
+        }
+    }
+}
+
+pub fn parse_script(input: &str, options: ParseOptions) -> ! {
     unimplemented!()
 }
-pub fn parse_module(input: &str) -> ! {
+pub fn parse_module(input: &str, options: ParseOptions) -> ! {
     unimplemented!()
 }
-pub fn parse_expr(input: &str) -> ! {
+pub fn parse_expr(input: &str, options: ParseOptions) -> ! {
     unimplemented!()
 }
 
