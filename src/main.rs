6d25e9deaba34cff0ff3dc6bc4c666623b4aeaf6
Nathan <nathan@users.noreply.github.com>
2017-07-17 09:45:06 -0400

    Defer usage printing

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
 use std::{env, process};
 
 fn run() -> Result<(), CliError> {
-    let file_name = env::args().nth(1).ok_or_else(|| {
-        print_usage();
-        CliError::MissingFileName
-    })?;
+    let file_name = env::args().nth(1).ok_or(CliError::MissingFileName)?;
     unimplemented!();
 }
 
@@ -19,6 +16,9 @@ enum CliError {
 fn main() {
     process::exit(match run() {
         Ok(_) => 0,
-        Err(_) => 1,
+        Err(CliError::MissingFileName) => {
+            print_usage();
+            1
+        },
     })
 }
