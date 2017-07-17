a77bb88c2b941ccc74df3e5435cd2bd86935e7aa
Nathan <nathan@users.noreply.github.com>
2017-07-17 09:50:10 -0400

    Read input file

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
-use std::{env, process};
+use std::{env, process, io, fs};
+use std::io::prelude::*;
 
 fn run() -> Result<(), CliError> {
     let file_name = env::args().nth(1).ok_or(CliError::MissingFileName)?;
+    let file = fs::File::open(file_name)?;
+    let mut buf_reader = io::BufReader::new(file);
+    let mut contents = String::new();
+    buf_reader.read_to_string(&mut contents)?;
     unimplemented!();
 }
 
@@ -11,14 +16,23 @@ fn print_usage() {
 
 enum CliError {
     MissingFileName,
+    Io(io::Error),
+}
+impl From<io::Error> for CliError {
+    fn from(inner: io::Error) -> CliError {
+        CliError::Io(inner)
+    }
 }
 
 fn main() {
     process::exit(match run() {
         Ok(_) => 0,
-        Err(CliError::MissingFileName) => {
-            print_usage();
+        Err(kind) => {
+            match kind {
+                CliError::MissingFileName => print_usage(),
+                CliError::Io(inner) => println!("{}", inner),
+            }
             1
-        },
+        }
     })
 }
