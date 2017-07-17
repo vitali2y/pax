56ff1f82c0d156942164a7df6567fbb9392df67a
Nathan <nathan@users.noreply.github.com>
2017-07-17 09:26:48 -0400

    Add stubs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
+pub fn parse_script(input: &str) -> ! {
+    unimplemented!()
+}
+pub fn parse_module(input: &str) -> ! {
+    unimplemented!()
+}
+pub fn parse_expr(input: &str) -> ! {
+    unimplemented!()
+}
+
 #[cfg(test)]
 mod tests {
 }
