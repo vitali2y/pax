2021662f67b50d4b472dc4c3040b43bb648c13b9
Nathan <nathan@users.noreply.github.com>
2017-07-17 09:38:57 -0400

    Refactor to use process::exit in the recommended way

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,23 @@
 use std::{env, process};
 
-fn print_usage() -> ! {
+fn print_usage() {
     println!("usage: esparse <file>");
-    process::exit(1);
+}
+
+fn run() -> Result<(), ()> {
+    let file_name = match env::args().nth(1) {
+        Some(f) => f,
+        None => {
+            print_usage();
+            return Err(())
+        },
+    };
+    unimplemented!();
 }
 
 fn main() {
-    let file_name = match env::args().nth(1) {
-        Some(f) => f,
-        None => print_usage(),
-    };
-    unimplemented!();
+    process::exit(match run() {
+        Ok(_) => 0,
+        Err(_) => 1,
+    })
 }
