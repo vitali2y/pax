89666c752fca438d8d74b24715cb8904565acf35
Nathan <nathan@users.noreply.github.com>
2017-07-26 15:02:37 -0400

    Document ast

--- esparse/src/ast.rs
+++ esparse/src/ast.rs
@@ -1,11 +1,18 @@
+/// A location in source code.
+///
+/// Stores both the bytewise [position](#structfield.pos) and the logical [line](#structfield.row) and [character](#structfield.col) numbers.
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Default)]
 pub struct Loc {
+    /// 0-based byte index.
     pub pos: usize,
+    /// 0-based line number.
     pub row: usize,
+    /// 0-based character number on the line.
     pub col: usize,
 }
 
 impl Loc {
+    /// Creates a new `Loc` with the given positions.
     #[inline]
     pub fn new(pos: usize, row: usize, col: usize) -> Self {
         Loc {
@@ -15,20 +22,30 @@ impl Loc {
         }
     }
 
+    /// Creates a new `Loc` pointing to the first byte of the source code (`pos`, `row`, and `col` all zero).
     #[inline]
     pub fn zero() -> Self {
         Default::default()
     }
 }
 
+/// A region of source code.
+///
+/// A pair of locations, representing a half-open range, and a file name, identifying the source code in which this region appears.
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub struct Span<'f> {
+    /// The name of the source code.
+    ///
+    /// Often a file name, but can be an arbitrary string like `<input>`.
     pub file_name: &'f str,
+    /// The (inclusive) starting location.
     pub start: Loc,
+    /// The (exclusive) ending location.
     pub end: Loc,
 }
 
 impl<'f> Span<'f> {
+    /// Creates a new `Span` with the given file name and locations.
     #[inline]
     pub fn new(file_name: &'f str, start: Loc, end: Loc) -> Self {
         Span {
@@ -38,11 +55,13 @@ impl<'f> Span<'f> {
         }
     }
 
+    /// Creates an empty `Span` at the given location, with the given file name.
     #[inline]
     pub fn empty(file_name: &'f str, loc: Loc) -> Self {
         Span::new(file_name, loc, loc)
     }
 
+    /// Creates an empty `Span` with the given file name, pointing to the first position in the file.
     #[inline]
     pub fn zero(file_name: &'f str) -> Self {
         Span::new(file_name, Default::default(), Default::default())
