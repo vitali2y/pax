06ff57ecc95319185fddc72d157ff353ec5d67d5
Nathan <nathan@users.noreply.github.com>
2017-07-26 15:05:11 -0400

    Fix str_lit_value of \\

--- esparse/src/lex.rs
+++ esparse/src/lex.rs
@@ -263,10 +263,10 @@ pub fn str_lit_value(source: &str) -> Result<Cow<str>, ParseStrLitError> {
     let len = range.len();
     let mut last_pos = 0;
     let mut got_bs = false;
-    for esc_pos in memchr::Memchr::new(b'\\', bytes) {
+    while let Some(bs_pos) = memchr::memchr(b'\\', &bytes[last_pos..]) {
         got_bs = true;
-        let bs_pos = esc_pos - 1;
         result.push_str(&range[last_pos..bs_pos]);
+        let esc_pos = bs_pos + 1;
         if esc_pos >= len {
             panic!("str_lit_value got '\\' at end of string");
         }
