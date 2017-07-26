a6e8a2a06b215c2882f07e3a0f7529823a4cc1c6
Nathan <nathan@users.noreply.github.com>
2017-07-26 15:57:35 -0400

    Add benches for --es-syntax and --es-syntax-everywhere

--- parcel/src/test.rs
+++ parcel/src/test.rs
@@ -14,3 +14,34 @@ fn bench_cjs_simple(b: &mut test::Bencher) {
         let _ = bundle(&entry_point, input_options, &output, &map_output).unwrap();
     });
 }
+
+#[bench]
+fn bench_es6_simple(b: &mut test::Bencher) {
+    let entry_point = Path::new("examples/es6-simple/index.mjs");
+    let input_options = InputOptions {
+        es6_syntax: true,
+        ..InputOptions::default()
+    };
+    let output = "/dev/null";
+    let map_output = SourceMapOutput::Inline;
+
+    b.iter(|| {
+        let _ = bundle(&entry_point, input_options, &output, &map_output).unwrap();
+    });
+}
+
+#[bench]
+fn bench_es6_everywhere_simple(b: &mut test::Bencher) {
+    let entry_point = Path::new("examples/es6-everywhere-simple/index.js");
+    let input_options = InputOptions {
+        es6_syntax: true,
+        es6_syntax_everywhere: true,
+        ..InputOptions::default()
+    };
+    let output = "/dev/null";
+    let map_output = SourceMapOutput::Inline;
+
+    b.iter(|| {
+        let _ = bundle(&entry_point, input_options, &output, &map_output).unwrap();
+    });
+}
