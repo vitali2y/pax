8a1d15dc355ca1c777731c7f8fbfbe24e7d6ee96
Nathan <nathan@users.noreply.github.com>
2018-07-20 14:36:02 -0400

    npm install dependencies in benchmarks if they're not there

--- pax/src/test.rs
+++ pax/src/test.rs
@@ -4,13 +4,33 @@
 extern crate test;
 
 use std::path::Path;
+use std::process;
 use super::*;
 
 cfg_if! {
     if #[cfg(feature = "bench")] {
+        fn npm_install(dir: &Path) {
+            let node_modules = dir.join("node_modules");
+            if node_modules.is_dir() { return }
+
+            let ok = process::Command::new("npm")
+                .arg("install")
+                .arg("--silent")
+                .current_dir(dir)
+                .spawn()
+                .expect("failed to start `npm install`")
+                .wait()
+                .unwrap()
+                .success();
+            if !ok {
+                panic!("`npm install` did not exit successfully");
+            }
+        }
+
         #[bench]
         fn bench_cjs_simple(b: &mut test::Bencher) {
             let entry_point = Path::new("examples/simple/index.js");
+            npm_install(entry_point.parent().unwrap());
             let input_options = InputOptions::default();
             let output = "/dev/null";
             let map_output = SourceMapOutput::Inline;
@@ -23,6 +43,7 @@ cfg_if! {
         #[bench]
         fn bench_es6_simple(b: &mut test::Bencher) {
             let entry_point = Path::new("examples/es6-simple/index.mjs");
+            npm_install(entry_point.parent().unwrap());
             let input_options = InputOptions {
                 es6_syntax: true,
                 ..InputOptions::default()
@@ -38,6 +59,7 @@ cfg_if! {
         #[bench]
         fn bench_es6_everywhere_simple(b: &mut test::Bencher) {
             let entry_point = Path::new("examples/es6-everywhere-simple/index.js");
+            npm_install(entry_point.parent().unwrap());
             let input_options = InputOptions {
                 es6_syntax: true,
                 es6_syntax_everywhere: true,
